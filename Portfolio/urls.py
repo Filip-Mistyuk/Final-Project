4c3ed7f901add5ebaee3576fc2c9a3fc5ad2eda3
Filip-Mistyuk <mistukfilipp@gmail.com>
2023-09-02 17:00:11 -0700

    MediaFix

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin 
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
-    path("project_rec/", include(("project_rec.urls", "project_rec"))),
-    path('', include(("home.urls"))), 
-]
+urlpatterns = (
+        [
+            path("admin/", admin.site.urls),
+            path("users/", include("users.urls")),
+            path("project_rec/", include(("project_rec.urls", "project_rec"))),
+            path('', include(("home.urls"))), 
+        ]    
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
