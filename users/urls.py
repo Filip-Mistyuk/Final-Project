a7da328f0044a7876fcec2a7802013de730c6bc4
Filip-Mistyuk <mistukfilipp@gmail.com>
2023-09-30 16:46:41 -0700

    register function

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from .views import RegisterView, user_exit,ShopLoginView
+
 urlpatterns = [
-
+    path("register/", RegisterView.as_view(), name="register"), 
+    path("logout/", user_exit, name="logout"),  
+    path("login/", ShopLoginView.as_view(), name="login"),
 ]
-
  
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.views.generic import CreateView, View
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponse
+
+
+class RegisterView(CreateView):
+    template_name = "users/register.html"
+    form_class = UserCreationForm
+    
+    def form_valid(self, form: UserCreationForm):
+        user = form.save()
+        login(self.request, user)
+        return redirect("/")
+
+
+class ShopLoginView(LoginView):
+    template_name = "users/login.html"
+    form_class = AuthenticationForm
+
+
+def user_exit(request):
+    logout(request)
+    return redirect("/")
